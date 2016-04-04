2a0222f0f318dfec799c8acc28c5affc86cbfe04
Jesse B. Hannah <jesse@jbhannah.net>
2016-04-04 15:45:10 -0700

    Test documentation

--- tests/unit/TinymceProcessorTest.php
+++ tests/unit/TinymceProcessorTest.php
@@ -1,9 +1,18 @@
 <?php
+/**
+ * TinyMCE Processor Test
+ *
+ * @copyright 2016 Cyberitas Technologies, LLC
+ * @license LGPL-3.0+
+ */
 namespace Cyberitas\TinymceProcessor\Tests\Unit;
 
 use Cyberitas\TinymceProcessor\Tests\TestCase;
 use Cyberitas\TinymceProcessor\TinymceProcessor;
 
+/**
+ * Test the TinyMCE processor.
+ */
 class TinymceProcessorTest extends TestCase
 {
     /**
@@ -19,27 +28,34 @@ class TinymceProcessorTest extends TestCase
     protected function setUp()
     {
         parent::setUp();
-        $this->mockWebApplication();
+        $this->mockApplication();
     }
 
     protected function _before()
     {
         $this->tmp = new TinymceProcessor();
-        $this->tmp->content = "https://www.youtube.com/watch?v=9bZkp7q19f0";
     }
 
+    /**
+     * TinymceProcessor extends yii\base\Model
+     */
     public function testExtendsYiiBaseModel()
     {
         $this->assertInstanceOf('yii\base\Model', $this->tmp);
     }
 
+    /**
+     * TinymceProcessor content goes through EssenceValidator and has valid URLs
+     * replaced with oEmbed media
+     */
     public function testRunsEssenceValidator()
     {
-        $content = <<<EOF
+        $this->tmp->content = "https://www.youtube.com/watch?v=9bZkp7q19f0";
+        $output = <<<EOF
 <iframe width="480" height="270" src="https://www.youtube.com/embed/9bZkp7q19f0?feature=oembed" frameborder="0" allowfullscreen></iframe>
 EOF;
 
         $this->assertTrue($this->tmp->validate());
-        $this->assertEquals($content, $this->tmp->content);
+        $this->assertEquals($output, $this->tmp->content);
     }
 }
